df888669285718b66455b9fa32c8341b44c959de
riungupatric <riungufx@gmail.com>
2022-05-13 15:09:45 +0300

    added logging for heroku

--- carzone/settings.py
+++ carzone/settings.py
@@ -174,3 +174,21 @@ EMAIL_USE_TLS = True
 
 #whitenoise setting for staticfiles in heroku
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+#heroku logging
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
